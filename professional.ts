5c6596eca5f1f8a145e21babff73687004b0729c
Meggy <146339540+chewchowtie@users.noreply.github.com>
2025-11-19 15:41:50 +0800

    Add files via upload

---
+++ professional.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professional',
+  imports: [],
+  templateUrl: './professional.html',
+  styleUrl: './professional.css',
+})
+export class Professional {
+
+}
