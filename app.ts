bb67f41f70216e54326e7b2af20281fb6c2cfe6e
Meggy <146339540+chewchowtie@users.noreply.github.com>
2025-11-19 15:42:48 +0800

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Skills } from "./skills/skills";
+import { Footer } from "./footer/footer";
+import { EducationalBackground } from './educational-background/educational-background';
+import { Professional } from "./professional/professional";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Skills, EducationalBackground, Footer, Professional],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
