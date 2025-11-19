36f8067e7175dd184e7cd4c54c593a31fe4aa59d
Meggy <146339540+chewchowtie@users.noreply.github.com>
2025-11-19 15:40:48 +0800

    Add files via upload

---
+++ educational-background.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-educational-background',
+  imports: [],
+  templateUrl: './educational-background.html',
+  styleUrl: './educational-background.css',
+})
+export class EducationalBackground {
+
+}
